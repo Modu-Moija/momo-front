fcca8ea477be117122c9c01fdc4cf583bfd48883
heeeun <gmldms784@naver.com>
2021-05-05 23:55:57 +0900

    make share button in header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,6 +5,10 @@ const Header = () => {
 	return (
 		<header>
 			<div className="title">MOMO</div>
+			<div className="share-con">
+				<button className="kakao-share">카카오톡 공유하기</button>
+				<button className="link-copy">링크 복사하기</button>
+			</div>
 		</header>
 	);
 };
