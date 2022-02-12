b44cb899e57a60aff51ce07de2e302c4d6488f1d
unknown <lsj8249@naver.com>
2022-02-12 12:17:14 +0900

    fix: key 추가

--- src/utils/NewLine.tsx
+++ src/utils/NewLine.tsx
@@ -5,14 +5,14 @@ type Props = {
 };
 
 export const NewLine = ({ newLineString }: Props) => {
-  return (
-    <div>
-      {newLineString.split("\n").map((txt) => (
-        <>
-          {txt}
-          <br />
-        </>
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			{newLineString.split("\n").map((txt, id) => (
+				<div key={id}>
+					{txt}
+					<br />
+				</div>
+			))}
+		</div>
+	);
 };
